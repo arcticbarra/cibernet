10c50d00ff71c2e5ba5e6e58ec1185cde05a7e4b
Enrique Barragan <ebarragan1997@gmail.com>
2017-04-11 17:43:07 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var mongoose = require('mongoose');
+// var config = require('./config/index');
+//var apiController = require('./controllers/apiController');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine','ejs');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// mongoose.connect(config.getDbConnectionString());
+//
+// apiController(app);
+
+app.listen(port);
---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function(){
+   $(".button-collapse").sideNav();
+   $('.slider').slider();
+   $(".dropdown-button").dropdown({ hover: false });
+})
+
+$('.active').css('background-color','rgb(17, 53, 107)')
